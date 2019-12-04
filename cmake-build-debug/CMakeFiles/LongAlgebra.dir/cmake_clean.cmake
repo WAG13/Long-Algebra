2e442a0cb21cec1af9012044849f9115b6cbf763
Velly69 <43824249+Velly69@users.noreply.github.com>
2019-12-05 00:31:21 +0200

    All functions are implemented

---
+++ cmake-build-debug/CMakeFiles/LongAlgebra.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/longalgebra.dir/1/Source.cpp.o"
+  "liblongalgebra.dll.a"
+  "longalgebra.exe"
+  "longalgebra.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/longalgebra.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
