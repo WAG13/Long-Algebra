925ffdc9c75df2d7949520cf01cfaf48aabe8831
Daryna Bond <35077916+WAG13@users.noreply.github.com>
2019-12-05 09:16:56 +0200

    fix

--- Long-Algebra/Demo/Demo.h
+++ Long-Algebra/Demo/Demo.h
@@ -7,7 +7,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "../BigNumber/BigNumber.h"
-#include "../EllipticCurves/ElipticCurve.h"
+//#include "../EllipticCurves/ElipticCurve.h"
 
 using std::cout;
 using std::cin;
